669e56651e511b62bec90e793aeaa2c16a5a4618
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2017-05-16 23:26:31 +0200

    Launching projectiles... very big ones.

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -21,17 +21,16 @@ void ATank::SetTurretReference(UTankTurret * TurretToSet)
 
 void ATank::Fire()
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Tank fired a projectile"), Time);
-
 	if (!Barrel) { return; }
 
 	// spawn a projectile at the socket lockation
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileBlueprint,
 		Barrel->GetSocketLocation(FName("Projectile")),
 		Barrel->GetSocketRotation(FName("Projectile"))
 	);
+
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
 
 // Sets default values
