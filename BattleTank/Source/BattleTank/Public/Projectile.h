7b834268a8d385d6f88994114176ac2561cf1686
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2017-07-07 21:59:38 +0200

    Now removing projectiles from the scene after they have hit the target.

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -22,6 +22,11 @@ public:
 private:
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
 
+	void OnTimerExpire();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float DestroyDelay = 10.f;
+
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
