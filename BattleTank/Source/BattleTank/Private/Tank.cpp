df7ed89b1a3d5c93692e5445dbcf0ddef1ef365d
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2017-07-09 23:04:52 +0200

    Working on Tank Damage

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -3,6 +3,23 @@
 #include "BattleTank.h"
 #include "Tank.h"
 
+float ATank::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCause)
+{
+	int32 DamagePoints = FPlatformMath::RoundToInt(DamageAmount);
+	int32 DamageToApply = FMath::Clamp(DamagePoints, 0, CurrentHealth);
+
+	UE_LOG(LogTemp, Warning, TEXT("DamageAmount=%f, DamageToApply=%i"), DamagePoints, DamageToApply);
+
+	CurrentHealth -= DamageToApply;
+
+	if(CurrentHealth <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank has died!!"));
+	}
+
+	return DamageToApply;
+}
+
 // Sets default values
 ATank::ATank()
 {
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -11,7 +11,19 @@ class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
+public:
+
+	//called by the engine when damage id dealt
+	virtual float TakeDamage (float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCause) override;
+
 	// Sets default values for this pawn's properties
 	ATank();
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	int32 StartingHealth = 100;
+
+	UPROPERTY(VisibleAnywhere, Category = "Health")
+	int32 CurrentHealth = StartingHealth;
 
 };
